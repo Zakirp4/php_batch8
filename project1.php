cc492c68581e7edad55c2bd4cd0d9bed1195a5f0
Zakir Hossain <zakirp4@gmail.com>
2025-05-07 23:37:28 +0600

    Update project1.php
    f

--- project1.php
+++ project1.php
@@ -1,5 +1,23 @@
 <?php
 
+$number = -4;
+$positive_negative_status = "";
 
+echo "The number -4 is" . positiveNegative($number) . " and" . oddEven($number); ;
+
+function positiveNegative($n) {
+    if($n > 0)
+        return " positive";
+    else
+        return " negative";
+  }
+
+
+  function oddEven($n) {
+    if($n % 2 == 0)
+        return " even";
+    else
+        return " odd";
+  }
 
 ?>
