beec57d3d1c5755fbdc6bf825d3d8510fb6a6b7d
Zakir Hossain <zakirp4@gmail.com>
2025-05-07 23:58:55 +0600

    project 4

--- project4.php
+++ project4.php
@@ -1,5 +1,16 @@
 <?php
-
-
-
-?>
+	$units = 275;
+	$bill = 0.0;
+	
+	if ($units <= 50) {
+	    $bill = $units * 3.50;
+	} elseif ($units <= 100) {
+	    $bill = (50 * 3.50) + (($units - 50) * 4.00);
+	} elseif ($units <= 200) {
+	    $bill = (50 * 3.50) + (50 * 4.00) + (($units - 100) * 5.20);
+	} else {
+	    $bill = (50 * 3.50) + (50 * 4.00) + (100 * 5.20) + (($units - 200) * 6.50);
+	}
+	
+	echo "For $units units, the Total electricity bill is: " . number_format($bill, 2);
+	?>
